09e485b57397ac21af10c4754394b38209b1f7e6
ldmnh <leducmanh123kt@gmail.com>
2024-12-27 01:16:34 +0700

    feat(search): add see more btn

---
+++ app/(default-layout)/(guest)/search-result/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function SearchResultPage() {
+  return (
+    <View>
+      <Text>SearchResultPage</Text>
+    </View>
+  );
+}
